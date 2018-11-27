f5274e6f191399fdd06cd09811ec07d531e30328
Wayne Stevens <wayne.stevens1@digital.homeoffice.gov.uk>
2018-11-27 15:48:06 +0000

    3rd and 4th specs passed - Decreases temp and min temp of 10oC
    Co-Authored-By: Will Wayman <nepeanwjdw@users.noreply.github.com>

--- jasmine-standalone-3.3.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.3.0/spec/thermostatSpec.js
@@ -9,8 +9,23 @@ describe("Thermostat", function() {
     expect(Thermostat.temp).toBe(20);
   });
 
-  it("increases temperature", function() {
+  it("increases temperature with an up function", function() {
     Thermostat.up();
     expect(Thermostat.temp).toBe(21);
   });
+
+  it("decreases temperature with a down function", function() {
+    Thermostat.down();
+    expect(Thermostat.temp).toBe(19);
+  });
+
+  it("has a minimum of 10 degrees", function() {
+      Thermostat.temp = 10;
+      Thermostat.down();
+      expect(Thermostat.temp).toBe(10);
+  });
+
+
+
+
 });
--- jasmine-standalone-3.3.0/src/thermostat.js
+++ jasmine-standalone-3.3.0/src/thermostat.js
@@ -1,6 +1,20 @@
 function Thermostat () {
   Thermostat.temp = 20;
+
+
+
   Thermostat.up = function() {
-    this.temp += 1;
+    this.temp ++;
   };
+
+  Thermostat.down = function() {
+      if ( this.temp > 10 ) {
+        this.temp --;
+      // }
+      // else {
+      //   alert( "Minimum temperature is 10 degree!")
+      };
+  };
+
+
 };
